979ffd70900d3f8701633f8da2d1762b462051a2
bannzai <kingkong999yhirose@gmail.com>
2020-01-28 21:15:12 +0900

    :memo: Add complex pattern of valid

---
+++ example/valid/complex/local/main.go
@@ -0,0 +1,32 @@
+package main
+
+type language int
+
+const (
+	golang language = iota
+	swift
+)
+
+func main() {
+	lang := golang
+	switch lang {
+	case golang:
+		println("golang")
+	case swift:
+		println("swift")
+	default:
+		println("default")
+	}
+}
+
+func a() {
+	lang := golang
+	switch lang {
+	case golang:
+		println("golang")
+	case swift:
+		println("swift")
+	default:
+		println("default")
+	}
+}
