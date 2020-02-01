37de71f0e5919477d1d424483f06613d5d87def3
bannzai <kingkong999yhirose@gmail.com>
2020-02-01 14:48:10 +0900

    :recycle: Fix input arguments

--- main.go
+++ main.go
@@ -6,15 +6,18 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 var verbose bool
 
+const argumentsSeparator = ","
+
 func main() {
 	var source string
 	var target string
-	flag.StringVar(&source, "source", "*.go", "Source go files are containing enum definition")
-	flag.StringVar(&target, "target", "*.go", "Target go files are containing to use enum")
+	flag.StringVar(&source, "source", "*.go", "Source go files are containing enum definition. Multiple specifications can be specified separated by ,. e.g) *.go,pkg/**/*.go ")
+	flag.StringVar(&target, "target", "*.go", "Target go files are containing to use enum. Multiple specifications can be specified separated by ,. e.g) *.go,pkg/**/*.go ")
 	flag.BoolVar(&verbose, "verbose", false, "Enabled verbose log")
 	flag.Parse()
 
@@ -22,22 +25,35 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	debugf("source file search start. path: %s\n", source)
-	sources, err := filepath.Glob(cwd + "/" + source)
-	if err != nil {
-		log.Fatal(err)
+	splitedSource := strings.Split(source, argumentsSeparator)
+	sources := []string{}
+	for _, source := range splitedSource {
+		foundPaths, err := filepath.Glob(cwd + "/" + source)
+		if err != nil {
+			log.Fatal(err)
+		}
+		sources = append(sources, foundPaths...)
 	}
 	debugf("source file search end: %v\n", sources)
+
 	debugf("target file search start. path: %s\n", target)
-	targets, err := filepath.Glob(cwd + "/" + target)
-	if err != nil {
-		log.Fatal(err)
+	splitedTarget := strings.Split(target, argumentsSeparator)
+	targets := []string{}
+	for _, target := range splitedTarget {
+		foundPaths, err := filepath.Glob(cwd + "/" + target)
+		if err != nil {
+			log.Fatal(err)
+		}
+		targets = append(targets, foundPaths...)
 	}
 	debugf("target file search end: %v\n", targets)
 
 	debugf("enum parse start: \n")
 	enums := parse(sources)
 	debugf("enum parse end: %v\n", enums)
+
 	if err := check(enums, targets); err != nil {
 		log.Fatal(err)
 	}
