b420fd1434594517adf70b3153da4a14879e9a76
bannzai <kingkong999yhirose@gmail.com>
2020-02-03 10:13:59 +0900

    :recycle: Refactor parser

--- parser.go
+++ parser.go
@@ -15,55 +15,125 @@ type enum struct {
 	patterns    []string
 }
 
+// parse traverse go file and these map to []enum.
+// ast strcture can be confirmed from here: https://play.golang.org/p/aitWi-5RoHj
 func parse(filepaths []string) []enum {
-	enums := []enum{}
+
+	type enumDeclInfo struct {
+		packagePath string
+		typeName    string
+	}
+
+	// NOTE: First, cache ast file
+	astFiles := []*ast.File{}
 	for _, path := range filepaths {
+		astFile := parseASTFile(path)
+		astFiles = append(astFiles, astFile)
+	}
+
+	// NOTE: Second, collect enum GenDecl Information
+	declInfos := []enumDeclInfo{}
+	for i, path := range filepaths {
+		astFile := astFiles[i]
+		ast.Inspect(astFile, func(node ast.Node) bool {
+			f, ok := node.(*ast.File)
+			if !ok {
+				// NOTE: It is support only global definition enum
+				return false
+			}
+
+			for _, decl := range f.Decls {
+				decl, ok := decl.(*ast.GenDecl)
+				if !ok {
+					continue
+				}
+				for _, spec := range decl.Specs {
+					spec, ok := spec.(*ast.TypeSpec)
+					if !ok {
+						continue
+					}
+					if identifier, ok := spec.Type.(*ast.Ident); ok {
+						declInfos = append(declInfos, enumDeclInfo{
+							packagePath: filepath.Dir(path),
+							typeName:    identifier.Name,
+						})
+					}
+				}
+			}
+			return true
+		})
+	}
+
+	// NOTE: Third, collect enum information with matched GenDecl informations.
+	enums := []enum{}
+	for i, path := range filepaths {
 		e := enum{}
 
-		astFile := parseASTFile(path)
+		astFile := astFiles[i]
 		e.packageName = astFile.Name.Name
 		e.packagePath = filepath.Dir(path)
 
 		ast.Inspect(astFile, func(node ast.Node) bool {
-			decl, ok := node.(*ast.GenDecl)
+			f, ok := node.(*ast.File)
 			if !ok {
-				return true
-			}
-			if decl.Tok != token.CONST {
-				return true
+				// NOTE: It is support only global definition enum
+				return false
 			}
 
-			patterns := []string{}
-			for _, spec := range decl.Specs {
-				valueSpec, ok := spec.(*ast.ValueSpec)
+			for _, decl := range f.Decls {
+				decl, ok := decl.(*ast.GenDecl)
 				if !ok {
 					continue
 				}
+				if decl.Tok != token.CONST {
+					continue
+				}
 
-				// e.g) golang language = iota
-				isFirstDeclearation := valueSpec.Type != nil && len(valueSpec.Values) > 0
-				if isFirstDeclearation {
-					ident, ok := valueSpec.Type.(*ast.Ident)
+				patterns := []string{}
+				for _, spec := range decl.Specs {
+					valueSpec, ok := spec.(*ast.ValueSpec)
 					if !ok {
 						continue
 					}
-					e.name = ident.Name
-				}
 
-				for _, name := range valueSpec.Names {
-					if name.Name == "_" {
+					// NOTE: e.g) golang language = iota
+					isFirstDeclearation := valueSpec.Type != nil && len(valueSpec.Values) > 0
+					if isFirstDeclearation {
+						ident, ok := valueSpec.Type.(*ast.Ident)
+						if !ok {
+							continue
+						}
+						for _, declInfo := range declInfos {
+							if declInfo.packagePath != e.packagePath {
+								continue
+							}
+							if declInfo.typeName != ident.Name {
+								continue
+							}
+						}
+						e.name = ident.Name
+					}
+
+					if e.name == "" {
+						debugf("e.name is empty. will patterns is %+v\n", valueSpec.Names)
 						continue
 					}
-					patterns = append(patterns, name.Name)
+
+					for _, name := range valueSpec.Names {
+						if name.Name == "_" {
+							continue
+						}
+						patterns = append(patterns, name.Name)
+					}
 				}
+				e.patterns = patterns
+
+				enums = append(enums, e)
 			}
-			e.patterns = patterns
-
-			enums = append(enums, e)
-
 			return true
 		})
 	}
+
 	return enums
 }
 
