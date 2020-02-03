bdc87963a4ccbcc8e5a896bfbe4ab4581d9800de
bannzai <kingkong999yhirose@gmail.com>
2020-02-03 10:24:24 +0900

    :recycle: Run each filepath for check

--- check.go
+++ check.go
@@ -16,101 +16,105 @@ type keepUsesInfo struct {
 }
 
 func check(enums []enum, filepaths []string) error {
+	debugf("filepaths: %+v\n", filepaths)
 	config := &packages.Config{Mode: packages.LoadSyntax}
-	pkgs, err := packages.Load(config, filepaths...)
-	if err != nil {
-		return err
-	}
-	debugf("pkgs: %v,\n", pkgs)
 
-	for _, pkg := range pkgs {
-		filepath := pkg.GoFiles[0]
-
+	for _, filepath := range filepaths {
 		debugf("start check %s\n", filepath)
 
-		info := pkg.TypesInfo
-		keepUsesInfos := []keepUsesInfo{}
-		for identifier, use := range info.Uses {
-			namedType, ok := use.Type().(*types.Named)
-			if !ok {
-				continue
-			}
-
-			for _, enum := range enums {
-				debugf("enum.name is %s, use enum type name is %s and start position %d\n", enum.name, namedType.Obj().Name(), identifier.Pos())
-				if enum.name != namedType.Obj().Name() {
-					continue
-				}
-				keepUsesInfos = append(keepUsesInfos, keepUsesInfo{enum: enum, startPosition: identifier.Pos()})
-			}
+		pkgs, err := packages.Load(config, filepath)
+		if err != nil {
+			return err
 		}
-		debugf("keep Uses infos %v \n", keepUsesInfos)
+		debugf("pkgs: %v,\n", pkgs)
 
-		for node, scope := range info.Scopes {
-			debugf("scope info: %+v\n", *scope)
+		for _, pkg := range pkgs {
+			debugf("package is: %+v, and path is %+v\n", pkg.Name, pkg.PkgPath)
 
-			switchNode, ok := node.(*ast.SwitchStmt)
-			if !ok {
-				continue
-			}
-			debugf("switchNode is %+v\n", switchNode)
-
-			for _, info := range keepUsesInfos {
-				if !scope.Contains(info.startPosition) {
+			info := pkg.TypesInfo
+			keepUsesInfos := []keepUsesInfo{}
+			for identifier, use := range info.Uses {
+				namedType, ok := use.Type().(*types.Named)
+				if !ok {
 					continue
 				}
 
-				// FIXME: It is difficult to tell about `switch x` ast.SwitchStmt or  `case xyz:` ast.SwitchStmt.
-				patternContainer := map[string]struct{}{}
-				for _, stmt := range switchNode.Body.List {
-					debugf("stmt is %v\n", stmt)
-					if caseClause, ok := stmt.(*ast.CaseClause); ok {
-						debugf("caseClause is %v\n", *caseClause)
-						for _, caseExpr := range caseClause.List {
-							debugf("caseExpr is %v, type of %v\n", caseExpr, reflect.TypeOf(caseExpr))
+				for _, enum := range enums {
+					debugf("enum.name is %s, use enum type name is %s and start position %d\n", enum.name, namedType.Obj().Name(), identifier.Pos())
+					if enum.name != namedType.Obj().Name() {
+						continue
+					}
+					keepUsesInfos = append(keepUsesInfos, keepUsesInfo{enum: enum, startPosition: identifier.Pos()})
+				}
+			}
+			debugf("keep Uses infos %v \n", keepUsesInfos)
 
-							func() {
-								// NOTE: this scope pack pattern names from external package
-								// FIXME: more explicit condition for extarct pattern name from external package
-								if caseValue, ok := caseExpr.(*ast.SelectorExpr); ok {
-									if packageInfo, ok := caseValue.X.(*ast.Ident); ok {
-										debugf("caseValue is %v, name of %v, x type is %v\n", caseValue, caseValue.Sel.Name, packageInfo.Name)
-										if packageInfo.Name == info.enum.packageName {
-											for _, pattern := range info.enum.patterns {
-												if pattern == caseValue.Sel.Name {
-													patternContainer[pattern] = struct{}{}
+			for node, scope := range info.Scopes {
+				debugf("scope info: %+v\n", *scope)
+
+				switchNode, ok := node.(*ast.SwitchStmt)
+				if !ok {
+					continue
+				}
+				debugf("switchNode is %+v\n", switchNode)
+
+				for _, info := range keepUsesInfos {
+					if !scope.Contains(info.startPosition) {
+						continue
+					}
+
+					// FIXME: It is difficult to tell about `switch x` ast.SwitchStmt or  `case xyz:` ast.SwitchStmt.
+					patternContainer := map[string]struct{}{}
+					for _, stmt := range switchNode.Body.List {
+						debugf("stmt is %v\n", stmt)
+						if caseClause, ok := stmt.(*ast.CaseClause); ok {
+							debugf("caseClause is %v\n", *caseClause)
+							for _, caseExpr := range caseClause.List {
+								debugf("caseExpr is %v, type of %v\n", caseExpr, reflect.TypeOf(caseExpr))
+
+								func() {
+									// NOTE: this scope pack pattern names from external package
+									// FIXME: more explicit condition for extarct pattern name from external package
+									if caseValue, ok := caseExpr.(*ast.SelectorExpr); ok {
+										if packageInfo, ok := caseValue.X.(*ast.Ident); ok {
+											debugf("caseValue is %v, name of %v, x type is %v\n", caseValue, caseValue.Sel.Name, packageInfo.Name)
+											if packageInfo.Name == info.enum.packageName {
+												for _, pattern := range info.enum.patterns {
+													if pattern == caseValue.Sel.Name {
+														patternContainer[pattern] = struct{}{}
+													}
 												}
 											}
 										}
 									}
-								}
-							}()
-							func() {
-								// NOTE: this scope pack pattern names from internal package
-								// FIXME: more explicit condition for extarct pattern name from internal package
-								if caseValue, ok := caseExpr.(*ast.Ident); ok {
-									debugf("caseValue is %v, name of %v\n", caseValue, caseValue.Name)
-									for _, pattern := range info.enum.patterns {
-										if pattern == caseValue.Name {
-											patternContainer[pattern] = struct{}{}
+								}()
+								func() {
+									// NOTE: this scope pack pattern names from internal package
+									// FIXME: more explicit condition for extarct pattern name from internal package
+									if caseValue, ok := caseExpr.(*ast.Ident); ok {
+										debugf("caseValue is %v, name of %v\n", caseValue, caseValue.Name)
+										for _, pattern := range info.enum.patterns {
+											if pattern == caseValue.Name {
+												patternContainer[pattern] = struct{}{}
+											}
 										}
 									}
-								}
-							}()
+								}()
+							}
 						}
 					}
-				}
-				debugf("all patterns %v\n", patternContainer)
+					debugf("all patterns %v\n", patternContainer)
 
-				for _, pattern := range info.enum.patterns {
-					debugf("pattern: %v\n", pattern)
+					for _, pattern := range info.enum.patterns {
+						debugf("pattern: %v\n", pattern)
 
-					if _, ok := patternContainer[pattern]; !ok {
-						position := switchNode.Switch
-						file := pkg.Fset.File(position)
-						line := file.Line(position)
-						filepath := pkg.GoFiles[0]
-						return fmt.Errorf("missing enum pattern for %s.%s.%s. at %s:%d:%d", info.enum.packageName, info.enum.name, pattern, filepath, line, position)
+						if _, ok := patternContainer[pattern]; !ok {
+							position := switchNode.Switch
+							file := pkg.Fset.File(position)
+							line := file.Line(position)
+							filepath := pkg.GoFiles[0]
+							return fmt.Errorf("missing enum pattern for %s.%s.%s. at %s:%d:%d", info.enum.packageName, info.enum.name, pattern, filepath, line, position)
+						}
 					}
 				}
 			}
