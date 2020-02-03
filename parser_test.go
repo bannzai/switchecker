bada3ba992618e1e6d5e098215249019c9beb604
bannzai <kingkong999yhirose@gmail.com>
2020-02-03 10:29:17 +0900

    :sparkles: Add test case

--- parser_test.go
+++ parser_test.go
@@ -99,6 +99,15 @@ func Test_parse(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "No support type defined struct",
+			args: args{
+				filepaths: []string{
+					testutil.CallerDirectoryPath(t) + "/testdata/parser/no_support_type_define_struct_and_interface.go",
+				},
+			},
+			want: []enum{},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
